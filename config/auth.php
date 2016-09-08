0cb5ec95b62dba019b65d49f2dec75530fdd4b6f
Tibor Hegedüs <developer@hegedustibor.hu>
2016-09-08 19:28:17 +0200

    Auth admin hozzáadása.

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -68,6 +73,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [

f5d9d483bd0d97a050103fdfcc2099dc10afd38d
Tibor Hegedüs <developer@hegedustibor.hu>
2016-09-28 17:02:44 +0200

    Forms & HTML collective telepítése.

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
 
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
